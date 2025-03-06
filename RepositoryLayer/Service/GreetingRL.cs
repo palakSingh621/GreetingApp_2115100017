9ec891a450990fd847136d51f74ed3eb624e3bfd
palakSingh621 <kushwahps621@gmail.com>
2025-03-06 15:25:37 +0530

    [Palak] Add . Greeting App Redis Cache Setup

--- RepositoryLayer/Service/GreetingRL.cs
+++ RepositoryLayer/Service/GreetingRL.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Metadata;
-using RepositoryLayer.Entity; 
+﻿using RepositoryLayer.Entity; 
 using RepositoryLayer.Context;
 using RepositoryLayer.Interface;
-using Microsoft.EntityFrameworkCore;
 
 namespace RepositoryLayer.Service
 {

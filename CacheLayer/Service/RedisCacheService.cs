d66520436cf1363528fd0495fe12a857a06194d5
palakSingh621 <kushwahps621@gmail.com>
2025-03-06 15:46:47 +0530

    Revert "[Palak] Refactor . Adding Redis Cache Setup files in Greeting App"
    This reverts commit 410e318da13525a243134a3eef7beff0e31e615b.
    
    "[Palak] reverting commits"

---
+++ CacheLayer/Service/RedisCacheService.cs
@@ -0,0 +1,34 @@
+﻿using CacheLayer.Interface;
+using StackExchange.Redis;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CacheLayer.Service
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDatabase _cacheDb;
+
+        public RedisCacheService(IConnectionMultiplexer redis)
+        {
+            _cacheDb = redis.GetDatabase();
+        }
+
+        public async Task<T?> GetCachedData<T>(string key)
+        {
+            var cachedData = await _cacheDb.StringGetAsync(key);
+            return cachedData.HasValue ? JsonSerializer.Deserialize<T>(cachedData) : default;
+        }
+
+        public async Task SetCachedData<T>(string key, T value, TimeSpan expiration)
+        {
+            var jsonData = JsonSerializer.Serialize(value);
+            await _cacheDb.StringSetAsync(key, jsonData, expiration);
+        }
+
+        public async Task<bool> RemoveCachedData(string key)
+        {
+            return await _cacheDb.KeyDeleteAsync(key);
+        }
+    }
+}

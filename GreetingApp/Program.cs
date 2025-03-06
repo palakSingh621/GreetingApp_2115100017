f9f6c8a7c775fccb811cb5a3539488e272553abf
palakSingh621 <kushwahps621@gmail.com>
2025-03-06 15:25:37 +0530

    [Palak] Add . Greeting App Redis Cache Setup

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -3,10 +3,13 @@ using BusinessLayer.Service;
 using RepositoryLayer.Interface;
 using RepositoryLayer.Service;
 using RepositoryLayer.Context;
+using CacheLayer.Interface;
+using CacheLayer.Service;
 using NLog;
 using NLog.Web;
 using Microsoft.EntityFrameworkCore;
 using Middleware.GlobalExceptionHandler;
+using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 logger.Info("Application Starting...");
@@ -27,6 +30,15 @@ builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Informatio
 builder.Host.UseNLog();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Redis configuration
+var redisConfig = builder.Configuration.GetSection("Redis");
+string redisConnectionString = $"{redisConfig["Host"]}:{redisConfig["Port"]}";
+
+// Register Redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnectionString));
+builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
